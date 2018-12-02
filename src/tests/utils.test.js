5650e2b642182bd92be052ff47381483c7b1a851
Nick Bansal <https://github.com/NickBansal>
2018-12-02 21:21:59 +0000

    Test added with Jest

---
+++ src/tests/utils.test.js
@@ -0,0 +1,11 @@
+import { createArray } from '../utils'
+
+
+describe('Util fucntions testing', () => {
+    it('Create Array functions creates a 2D array with a specified amount', () => {
+        expect(createArray(1)).toEqual([[0]])
+        expect(createArray(2)).toEqual([[0, 0], [0, 0]])
+        expect(createArray(3)).toEqual([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
+        expect(createArray(6)).toHaveLength(6)
+    })
+})
---
+++ src/utils/index.js
@@ -0,0 +1,7 @@
+export const createArray = rows => {
+    const arr = [];
+    for (let i = 0; i < rows; i++) {
+        arr[i] = Array(rows).fill(0)
+    }
+    return arr;
+}
